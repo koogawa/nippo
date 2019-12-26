e98d7dccc259d0f917ab0b9152dfdd77a404c20f
koogawa <koogawa.app@gmail.com>
2019-12-26 21:07:26 +0900

    First commit

---
+++ components/Logo.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1><img class="logo" src="~/assets/title.png"></h1>
+</template>
+<style>
+.logo {
+  animation: 1s appear;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
